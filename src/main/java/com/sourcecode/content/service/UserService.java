18e4f5d4c469e4597993aadd7e917acb23df9037
ifredom <1950735817@qq.com>
2022-09-15 16:26:11 +0800

    feat: 6.使用Java-jdk的动态代理类Proxy,InvocationHandle实现AOP切面功能

---
+++ src/main/java/com/sourcecode/content/service/UserService.java
@@ -0,0 +1,14 @@
+package com.sourcecode.content.service;
+
+/**
+ * @Author ifredomvip@gmail.com
+ * @Date 2022/9/15 15:28
+ * @Version 1.0.0
+ * @Description
+ **/
+public interface UserService {
+    /**
+     * 测试输出
+     */
+    void test();
+}
---
+++ src/main/java/com/sourcecode/spring/aop/AopProxyHandle.java
@@ -0,0 +1,44 @@
+package com.sourcecode.spring.aop;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+/**
+ * @Author ifredomvip@gmail.com
+ * @Date 2022/9/15 15:03
+ * @Version 1.0.0
+ * @Description
+ **/
+public class AopProxyHandle implements InvocationHandler {
+
+    private Object target;
+
+    public AopProxyHandle(Object target) {
+        this.target = target;
+    }
+
+    public void setTarget(Object target) {
+        this.target = target;
+    }
+
+    public Object gerProxy() {
+        return Proxy.newProxyInstance(this.getClass().getClassLoader(), target.getClass().getInterfaces(), this);
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        before();
+        Object result = method.invoke(target, args);
+        after();
+        return result;
+    }
+
+    private void before() {
+//        System.out.println("proxy action before...");
+    }
+
+    private void after() {
+//        System.out.println("proxy action after...");
+    }
+}
